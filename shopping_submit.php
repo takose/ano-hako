796889b984f4438d6588292d7816e19f3f5d272f
Rina Takase <rina.m.takase@gmail.com>
2017-01-08 16:18:36 +0900

    tateoka/[WIP]implement buy products function

---
+++ shopping_submit.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+  $productname["コーラ"] = "コーラ";
+  $productname["コーヒー"] = "コーヒー";
+  $productname["ファンタオレンジ"] = "ファンタオレンジ";
+  $productname["ファンタグレープ"] = "ファンタグレープ";
+  $productname["サイダー"] = "サイダー";
+  $productname["オレンジジュース"] = "オレンジジュース";
+
+  $username["tateoka"] = "楯岡さん";
+  $username["takase"] = "高瀬さん";
+  $username["nakazato"] = "中里さん";
+  $username["kuroki"] = "黒木さん";
+
+
+$pdo = new PDO("sqlite:anohako.sqlite");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+//在庫の表示
+// $st = $pdo->query("select * from product order by id desc");
+// $product = $st->fetchAll();
+
+$Pname = $_GET["productname"];
+$st = $pdo->query("select price from product where name = '" . $Pname . "';");
+$price = $st->fetchAll();
+
+//$Pnumber = $stock[0]["number"];
+// $st = $pdo->query("select number from zaiko where id = '" . $Pid . "';");
+// $number = $st->fetchAll();
+
+$Uname = $_GET["username"];
+$st = $pdo->query("select money from user where name = '" . $Uname . "';");
+$money = $st->fetchAll();
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>My Blog - コメント登録</title>
+
+  </head>
+  <body>
+
+    <?php
+    if(isset($_GET["productname"])){
+      //残金の定義
+      $money[0]['money'] = $money[0]['money'] - $price[0]['price'];
+
+      print $productname[$Pname] . "を買いました。";
+      print '<br>';
+      print $username[$Uname] . "の所持金は" . $money[0]['money'] . "円になりました";
+      //print $Pnumber;
+  }
+      ?>
+
+　　<p class="article_link"><a href="toppage.html">トップページに戻る</a></p>
+
+  </body>
+</html>
--- toppage.php
+++ toppage.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$username["tateoka"] = "楯岡さん";
-$username["takase"] = "高瀬さん";
-$username["nakazato"] = "中里さん";
-$username["kuroki"] = "黒木さん";
+  $username["tateoka"] = "楯岡さん";
+  $username["takase"] = "高瀬さん";
+  $username["nakazato"] = "中里さん";
+  $username["kuroki"] = "黒木さん";
 
 $pdo = new PDO("sqlite:anohako.sqlite");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
@@ -11,10 +11,9 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $st = $pdo->query("select * from product order by id desc");
 $product = $st->fetchAll();
 
-$name = $_GET["name"];
-$st = $pdo->query("select money from user where name = '" . $name . "';");
+$Uname = $_GET["username"];
+$st = $pdo->query("select money from user where name = '" . $Uname . "';");
 $money = $st->fetchAll();
-
 ?>
 
 <!DOCTYPE html>
@@ -22,37 +21,45 @@ $money = $st->fetchAll();
   <head>
     <meta charset="utf-8">
     <title>Ano-hako-database</title>
-<!-- <link rel="stylesheet" href="myblog_style.css"> -->
+
   </head>
   <body>
     <pre>
-<?php
-if(isset($_GET["name"])){
-  //[0]なんとかする
-  print $username[$name] . "の所持金は" . $money[0]['money'];
+<form action="shopping_submit.php" method="get">
+      <?php
+      if(isset($_GET["username"])){
 
-  // foreach($data3 as $meney){
-  //   print
-  // }
-}
-?>
-<table>
-<?php
-foreach($product as $p) {
-  print '<tr>';
-  print '<th>商品名:' .$p["name"] . '</th>';
-  print '<th>値段:' . $p["price"] . '</th>';
+        print $username[$Uname] . "の所持金は" . $money[0]['money']. "円です";
+        print '<input type="hidden" name="username" value='.$_GET["username"].' >';
+     }
+      ?>
+      <table>
+        <?php
 
-  $st = $pdo->query("select * from zaiko where id =" .$p["id"] . ";");
-  $stock = $st->fetchAll();
-  print '<td>商品の数:'.$stock[0]["number"].'</td>';
-  print '<td>商品番号:'.$stock[0]["priceid"].'</td>';
-  print '<td>購入</td>'
-  print '</tr>';
-}
-?>
-</table>
-     </pre>
 
+        foreach($product as $p) {
+          print '<tr>';
+          print '<th>商品名:' .$p["name"] . '</th>';
+          print '<th>値段:' . $p["price"] . '円</th>';
+          $st = $pdo->query("select * from zaiko where id =" .$p["id"] . ";");
+          $stock = $st->fetchAll();
+          print '<td>商品の数:'.$stock[0]["number"].'個</td>';
+          print '<td>商品番号:'.$stock[0]["priceid"].'</td>';
+          //print '<input type="hidden" name="number" value='.$stock[0]["number"].' >';
+          print '</tr>';
+        }
+        ?>
+
+     </table>
+     <form action="shopping_submit.php" method="get"> 商品を選択してください<br>
+             <input type="checkbox" name="productname" value="コーラ">コーラ
+             <input type="checkbox" name="productname" value="コーヒー">コーヒー
+             <input type="checkbox" name="productname" value="ファンタオレンジ">ファンタオレンジ
+             <input type="checkbox" name="productname" value="ファンタグレープ">ファンタグレープ
+             <input type="checkbox" name="productname" value="サイダー">サイダー
+             <input type="checkbox" name="productname" value="オレンジジュース">オレンジジュース
+<input type="submit" value="購入">
+</form>
+   </pre>
   </body>
 </html>
