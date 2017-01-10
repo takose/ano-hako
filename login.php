03d71ad78df426cde0553c9b580f8d142133d48c
Rina Takase <rina.m.takase@gmail.com>
2017-01-10 20:30:50 +0900

    add links and adjust view

--- login.php
+++ login.php
@@ -37,19 +37,23 @@ header('Content-Type: text/html; charset=UTF-8');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/4.1.0/sanitize.min.css">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <link rel="stylesheet" href="./style.css">
   <title></title>
 </head>
 <body>
-  <h1>Login</h1>
+  <div class="container">
+  <h2>Login</h2>
   <form method="post" action="">
-    <input type="text" name="username" placeholder="username" value="">
-    <input type="password" name="password" placeholder="password" value="">
-    <input type="hidden" name="token" value="<?=h(generate_token())?>">
-    <input type="submit" value="buy drink">
+    <input type="text" class="login_form" name="username" placeholder="username" value="">
+    <input type="password" class="login_form" name="password" placeholder="password" value="">
+    <input type="hidden" name="token" value="<?=h(generate_token())?>"><br>
+    <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect buy" type="submit" value="buy drink">
   </form>
   <?php if (http_response_code() === 403): ?>
     <p style="color: red;">ユーザ名またはパスワードが違います</p>
   <?php endif; ?>
   <?php print $pass[0]['password'] ?>
+  </div>
 </body>
 </html>
